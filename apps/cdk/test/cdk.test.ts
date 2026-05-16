ec93b92719fc9a15bbab6c48f886e2522d87b12d
Koshi Futami <52932398+KoshiFutami@users.noreply.github.com>
2026-05-16 23:19:22 +0900

    fix(cdk): allow profile image storage prefixes (#48)

--- apps/cdk/test/cdk.test.ts
+++ apps/cdk/test/cdk.test.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { Match, Template } from 'aws-cdk-lib/assertions';
 import { WanSnapInfrastructureStack } from '../lib/cdk-stack';
 
 describe('WanSnapInfrastructureStack', () => {
@@ -10,5 +10,52 @@ describe('WanSnapInfrastructureStack', () => {
     const template = Template.fromStack(stack);
 
     expect(template.toJSON()).toHaveProperty('Parameters');
+  });
+
+  it('grants the API storage user access to all image prefixes', () => {
+    const app = new cdk.App();
+    const stack = new WanSnapInfrastructureStack(app, 'TestStack');
+
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: ['s3:PutObject', 's3:DeleteObject', 's3:GetObject'],
+            Effect: 'Allow',
+            Resource: [
+              Match.objectLike({
+                'Fn::Join': [
+                  '',
+                  [
+                    Match.anyValue(),
+                    '/posts/*',
+                  ],
+                ],
+              }),
+              Match.objectLike({
+                'Fn::Join': [
+                  '',
+                  [
+                    Match.anyValue(),
+                    '/avatars/*',
+                  ],
+                ],
+              }),
+              Match.objectLike({
+                'Fn::Join': [
+                  '',
+                  [
+                    Match.anyValue(),
+                    '/dogs/*',
+                  ],
+                ],
+              }),
+            ],
+          },
+        ],
+      },
+    });
   });
 });
